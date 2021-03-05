0e0b198493a511dac85b52643cd32e1b4d4f9f55
kero <kero@comore.bi>
2021-03-05 19:43:42 +0900

    setup Bullet

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,4 +68,14 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.after_initialize do
+    Bullet.enable        = true
+    Bullet.alert         = false
+    Bullet.bullet_logger = true
+    Bullet.console       = true
+    # Bullet.growl       = true
+    Bullet.rails_logger  = true
+    Bullet.add_footer    = true
+  end
 end
