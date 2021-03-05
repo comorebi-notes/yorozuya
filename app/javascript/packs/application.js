e26421bb301d259ff75c7dfeaa06adbaae28eb71
kero <kero@comore.bi>
2021-03-05 19:00:03 +0900

    webpacker を chunk で分割

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,24 +3,24 @@ import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 import 'fetch-polyfill'
 
-// Libraries
-import $ from 'jquery'
-import 'bootstrap'
-import '@fortawesome/fontawesome-free/js/all'
-
-global.$ = $
-global.jQuery = $
-
-require('@nathanvda/cocoon')
-
-// Sass
-import '../src/stylesheets/application.sass'
-
 // Rails
 import Rails from '@rails/ujs'
 import Turbolinks from 'turbolinks'
 import * as ActiveStorage from '@rails/activestorage'
 import 'channels'
+
+// Libraries
+import $ from 'jquery'
+import 'bootstrap'
+import '@fortawesome/fontawesome-free/js/all'
+
+// Sass
+import '../src/stylesheets/application.sass'
+
+global.$ = $
+global.jQuery = $
+
+require('@nathanvda/cocoon')
 
 Rails.start()
 Turbolinks.start()
