17492e9172bb9f4115b040186f46dd4f37d073b2
kero <kero@comore.bi>
2021-03-19 23:47:14 +0900

    レイアウトを微調整

--- app/javascript/scripts/admin/vendor/fonts.js
+++ app/javascript/scripts/admin/vendor/fonts.js
@@ -1,11 +1,29 @@
 // Adobe Fonts
 // https://fonts.adobe.com/my_fonts?project_id=nsw3pxe#web_projects-section
 
-(function(d) {
-  var config = {
-    kitId: 'nsw3pxe',
-    scriptTimeout: 3000,
-    async: true
-  },
-  h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-})(document);
+document.addEventListener('turbolinks:load', () => {
+  (function(d) {
+    var config = {
+        kitId: 'nsw3pxe',
+        scriptTimeout: 3000,
+        async: true
+      },
+      h = d.documentElement, t = setTimeout(function() {
+        h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive";
+      }, config.scriptTimeout), tk = d.createElement("script"), f = false, s = d.getElementsByTagName("script")[0], a;
+    h.className += " wf-loading";
+    tk.src = 'https://use.typekit.net/' + config.kitId + '.js';
+    tk.async = true;
+    tk.onload = tk.onreadystatechange = function() {
+      a = this.readyState;
+      if (f || a && a != "complete" && a != "loaded") return;
+      f = true;
+      clearTimeout(t);
+      try {
+        Typekit.load(config)
+      } catch (e) {
+      }
+    };
+    s.parentNode.insertBefore(tk, s)
+  })(document);
+})
